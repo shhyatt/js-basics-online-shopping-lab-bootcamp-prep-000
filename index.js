64f6f4244a7adff4957ccc2b32d7cdf5a48258c5
Samantha Hyatt <shhyatt@gmail.com>
2018-11-01 16:22:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
-  var price = Math.floor(Math.random() * 100); 
-  cart.push({ [item] : price});
-  console.log(`${item} has been added to your cart.`);
+
+function addToCart(itemName) {
+  
+  var itemPrice = Math.floor(Math.random()*100); 
+  cart.push(new Object ({itemName : itemPrice})); 
+
+  console.log(`${itemName} has been added to your cart.`);
   return cart;
 
   
@@ -21,30 +24,6 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  if (cart.length === 0) {
-    return console.log("Your cart is empty");
-  }
-  var string = "In your cart you have ";
-  if (cart.length === 1) {
-    string += `${Object.keys(cart[0]) } at $${cart[0]
-    [Object.keys(cart[0])]}.`;
-  } else if (cart.length === 2) {
-    string += `${Object.keys(cart[0]) } at $${cart[0]
-    [Object.keys(cart[0])]} and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}.`
-  } else {
-    for (var i=0; i<cart.length; i++) {
-      var text; 
-      if (i === cart.length - 2) {
-        text = ", and ";
-      } else if (i === cart.length - 1) {
-        text = "";
-      } else {
-        text = ", ";
-      }
-      string += `${Object.keys(cart[i])} at $${cart[i]
-      [Object.keys(cart[i])]}` + text + `${i === cart.length - 1 ? '.' : ''}`;
-    }
-  }
   
   return console.log(string);
 }
